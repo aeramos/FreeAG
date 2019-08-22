bfe748e7660908d64ea056a4b54ca8bb7b7daf36
Alejandro Ramos <aeramos.work@gmail.com>
2019-08-22 00:40:49 -0400

    Add Item and Store
    Neither of them are nearly finished. They'll both be improved as the
    development of the game progresses.

---
+++ src/Item.java
@@ -0,0 +1,44 @@
+/*
+ * This file is part of FreeAG.
+ * Copyright (C) 2019 Alejandro Ramos
+ *
+ * FreeAG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FreeAG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with FreeAG.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+public class Item {
+    // will add more properties as needed and as the game expands
+    private int id;
+    private String name;
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/Store.java
@@ -0,0 +1,42 @@
+/*
+ * This file is part of FreeAG.
+ * Copyright (C) 2019 Alejandro Ramos
+ *
+ * FreeAG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FreeAG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with FreeAG.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+public class Store {
+    private String name;
+    private Item[] items;
+
+    public Store(String name, Item[] items) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Item[] getItems() {
+        return items;
+    }
+
+    public void setItems(Item[] items) {
+        this.items = items;
+    }
+}
